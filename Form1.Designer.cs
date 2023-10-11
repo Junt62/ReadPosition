77ed88803f4db92c84d5dd38cad1f2771dc0bfb1
Junt62 <junt62@qq.com>
2023-10-11 12:40:11 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace ReadPosition {
+    partial class Form1 {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(384, 261);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "读取并生成坐标文件中...";
+            this.TopMost = true;
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReadPosition {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        public void Form1_Shown(object sender, EventArgs e) {
+            string path = Program.CommandLineArgs;
+            //path = "C:\\Users\\Administrator\\Downloads\\a.txt";
+            string pathDirectory = Path.GetDirectoryName(path);
+
+            if (path != null) {
+                string pathExtension = Path.GetExtension(path).ToLower();
+                if (!Array.Exists(new string[] { ".txt", ".xml" }, ext => ext == pathExtension)) {
+                    MessageBox.Show("此程序只接受 txt xml 格式的文件", "错误", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                    Application.Exit();
+                    return;
+                }
+            }
+
+            string pattern = "<Entry Path=\"([^\"]*)\"[^>]*ofsx=\"([^\"]*)\"[^>]*ofsy=\"([^\"]*)\"[^>]*>";
+            List<EntryData> extractedData = ExtractDataFromFile(path, pattern);
+            if (extractedData != null) {
+                foreach (EntryData data in extractedData) {
+                    string fileNameWithoutExt = Path.GetFileNameWithoutExtension(data.Path);
+                    string fileName = Path.Combine("Placements", fileNameWithoutExt + ".txt");
+                    string fileDirectory = Path.GetDirectoryName(data.Path).Substring(2);
+                    string filePath = pathDirectory + Path.Combine(fileDirectory, fileName);
+                    if (!Directory.Exists(Path.GetDirectoryName(filePath))) {
+                        Directory.CreateDirectory(Path.GetDirectoryName(filePath));
+                    }
+
+                    using (StreamWriter writer = new StreamWriter(filePath)) {
+                        writer.WriteLine(data.Ofsx);
+                        writer.WriteLine(data.Ofsy);
+                    }
+
+                }
+                MessageBox.Show("已在文件路径生成坐标文件", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Application.Exit();
+            }
+            else {
+                MessageBox.Show("未找到任何可供生成的数据", "错误", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                Application.Exit();
+            }
+
+        }
+
+        static string[] FindLinesWithPattern(string path, string pattern) {
+            try {
+                string fileContent = File.ReadAllText(path);
+
+                MatchCollection matches = Regex.Matches(fileContent, pattern);
+
+                if (matches.Count > 0) {
+                    string[] matchedLines = new string[matches.Count];
+                    for (int i = 0; i < matches.Count; i++) {
+                        matchedLines[i] = matches[i].Value;
+                    }
+                    return matchedLines;
+                }
+            }
+            catch (ArgumentNullException) {
+                MessageBox.Show("请拖动 txt xml 格式的文件至本程序图标打开" +
+                    "\n" +
+                    "\n将在文件路径下生成 Placements 坐标文件", "提示", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            catch (FileNotFoundException) {
+                MessageBox.Show("文件不存在，可能被删除或移动", "错误", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message, "错误", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+            return null;
+        }
+
+        static List<EntryData> ExtractDataFromFile(string filePath, string pattern) {
+
+            List<EntryData> extract = new List<EntryData>();
+
+            try {
+                string content = File.ReadAllText(filePath);
+
+                MatchCollection matches = Regex.Matches(content, pattern);
+
+                foreach (Match match in matches) {
+                    string path = match.Groups[1].Value;
+                    string ofsx = match.Groups[2].Value;
+                    string ofsy = match.Groups[3].Value;
+
+                    EntryData data = new EntryData {
+                        Path = path,
+                        Ofsx = ofsx,
+                        Ofsy = ofsy
+                    };
+
+                    extract.Add(data);
+                }
+            }
+            catch (ArgumentNullException) {
+                MessageBox.Show("请拖动 txt xml 格式的文件至本程序图标打开" +
+                    "\n" +
+                    "\n将在文件路径下生成 Placements 坐标文件", "提示", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return null;
+            }
+            catch (FileNotFoundException) {
+                MessageBox.Show("文件不存在，可能被删除或移动", "错误", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                return null;
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message, "错误", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                return null;
+            }
+
+            return extract;
+        }
+    }
+
+}
+
+class EntryData {
+    public string Path { get; set; }
+    public string Ofsx { get; set; }
+    public string Ofsy { get; set; }
+
+    public EntryData() { }
+}
+
