260ecfeead90f92daaf2d4c8218cc1fcf15bcbbc
Junt62 <junt62@qq.com>
2023-10-11 12:51:55 +0800

    updated.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,12 @@
             // 
             // Form1
             // 
-            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.ClientSize = new System.Drawing.Size(384, 261);
             this.DoubleBuffered = true;
             this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "读取并生成坐标文件中...";
             this.TopMost = true;
             this.Shown += new System.EventHandler(this.Form1_Shown);
